846435b77023e8b528403cff8b7336a1090faf6c
Magnus Fulton <magnus@magnusfulton.com>
2026-06-10 10:36:59 -0700

    Updated the script to download wav files

--- vox-tiny/download-files.sh
+++ vox-tiny/download-files.sh
@@ -6,6 +6,6 @@ wget https://s3.magnusfulton.com/shared/labrador/vosk-model-small-en-us-0.15.zip
 unzip vosk-model-small-en-us-0.15.zip
 rm vosk-model-small-en-us-0.15.zip
 
-wget https://s3.magnusfulton.com/shared/labrador/intents-spoken.zip
-unzip intents-spoken.zip
-rm intents-spoken.zip
+wget https://s3.magnusfulton.com/shared/labrador/intents-spoken-wav.zip
+unzip intents-spoken-wav.zip
+rm intents-spoken-wav.zip
